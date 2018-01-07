e2e382c21d6202b0e384e3963cba336b12b6974d
Fabiano Cunha <cunhasb@aol.com>
2018-01-07 14:32:51 -0500

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Todo extends Component {
+  handleOnClick = () => {
+    this.props.store.dispatch({ type: "DELETE_TODO", id: this.props.id });
+  };
   render() {
     return (
       <li>
         {this.props.text}
+        <button onClick={this.handleOnClick}>
+          <img
+            src="http://agnesday.com/wp-content/uploads/2012/05/deletered.png"
+            alt="Delete"
+            width="10px"
+          />
+        </button>
       </li>
     );
   }
-};
+}
 
-export default Todo
+export default Todo;
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,14 +1,25 @@
 let id = 0;
 
-export default function manageTodo(state = {
-  todos: []
-}, action) {
+export default function manageTodo(
+  state = {
+    todos: []
+  },
+  action
+) {
   switch (action.type) {
-    case 'ADD_TODO':
+    case "ADD_TODO":
       id++;
       const todo = Object.assign({}, action.todo, { id: id });
       return { todos: state.todos.concat(todo) };
+    case "DELETE_TODO":
+      const todo_index = state.todos.findIndex(todo => todo.id === action.id);
+      return {
+        todos: [
+          ...state.todos.slice(0, todo_index),
+          ...state.todos.slice(todo_index + 1)
+        ]
+      };
     default:
       return state;
   }
-};
+}
